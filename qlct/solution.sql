1d7dfe17c7b3a48179836c61924334c092026fe3
Kim Minh Thắng <thangved02@gmail.com>
2022-11-14 12:19:00 +0700

    Update solution.sql

--- qlct/solution.sql
+++ qlct/solution.sql
@@ -5,6 +5,7 @@
 --------------------------------------------------------------------------------
 
 
+
 -- 1.1 -------------------------------------------------------------------------
 
 select b.tenct, b.diachict
@@ -346,3 +347,228 @@ where
         from congtrinh
     )
 order by a.mscn;
+
+--------------------------------------------------------------------------------
+-- 3.1 -------------------------------------------------------------------------
+
+select a.msct, a.tenthau, sum(b.kinhphi) tongkp
+from chuthau a, congtrinh b
+where a.msct = b.msct
+group by a.msct, a.tenthau
+order by a.msct;
+
+-- 3.2 -------------------------------------------------------------------------
+
+select a.mskts, a.hotenkts, sum(b.thulao) sotien
+from kientrucsu a, thietke b
+where a.mskts = b.mskts
+group by a.mskts, a.hotenkts
+having sum(b.thulao) > 25
+order by a.mskts;
+
+-- 3.3 -------------------------------------------------------------------------
+
+create view temp3 as
+select a.mskts
+from kientrucsu a, thietke b
+where a.mskts = b.mskts
+group by a.mskts
+having sum(b.thulao) > 50;
+
+select count(mskts) sokts
+from temp3;
+
+drop view temp3;
+
+-- 3.4 -------------------------------------------------------------------------
+
+select a.sttct, a.tenct, count(b.mscn) socn
+from congtrinh a, thamgia b
+where b.sttct = a.sttct
+group by a.sttct, a.tenct
+order by a.sttct;
+
+-- 3.5 -------------------------------------------------------------------------
+
+create view temp5 as
+select a.tenct, a.diachict, count(b.mscn) socn
+from congtrinh a, thamgia b
+where b.sttct = a.sttct
+group by a.tenct, a.diachict;
+
+select tenct, diachict
+from temp5
+where socn = (
+  select max(socn)
+  from temp5
+);
+
+drop view temp5;
+
+-- 3.6 -------------------------------------------------------------------------
+
+select tinhthanh, to_char(avg(kinhphi), '9999.99') kptb
+from congtrinh
+group by tinhthanh
+having avg(kinhphi) > 1000;
+
+-- 3.7 -------------------------------------------------------------------------
+
+select a.mscn, a.hotencn, sum(b.songay)
+from congnhan a, thamgia b
+where a.mscn = b.mscn
+group by a.mscn, a.hotencn
+having sum(b.songay) > (
+  select sum(d.songay)
+  from congnhan c, thamgia d
+  where d.mscn = c.mscn
+  and c.hotencn = 'Nguyen Hong Van'
+);
+
+
+-- 3.8 -------------------------------------------------------------------------
+
+select b.msct, b.tenthau, a.tinhthanh, count(a.sttct) soct
+from congtrinh a, chuthau b
+where a.msct = b.msct
+      and b.tenthau like '% Van %'
+group by a.tinhthanh, b.msct, b.tenthau
+order by b.msct;
+
+-- 3.9 -------------------------------------------------------------------------
+
+select a.mscn, a.hotencn
+from congnhan a, thamgia b
+where a.mscn = b.mscn
+group by a.mscn, a.hotencn
+having count(b.sttct) > 3;
+
+-- 3.10 ------------------------------------------------------------------------
+
+select a.hotencn, b.hotencn
+from congnhan a, congnhan b, thamgia c, thamgia d
+where a.mscn < b.mscn
+      and a.mscn = c.mscn
+      and b.mscn = d.mscn
+      and c.sttct = d.sttct
+group by a.mscn, b.mscn, a.hotencn, b.hotencn
+having count(c.sttct) >= 2;
+      
+--------------------------------------------------------------------------------
+
+-- 4.1 -------------------------------------------------------------------------
+
+select distinct a.hotenkts, a.noitn
+from kientrucsu a, thietke b, congtrinh c
+where a.hotenkts like 'Le %'
+      and a.mskts = b.mskts
+      and b.sttct = c.sttct
+      and c.tinhthanh = some (
+        select d.tinhthanh
+        from congtrinh d, chuthau e
+        where d.msct = e.msct
+              and e.tenthau = 'Hoang Cong Binh'
+      );
+      
+-- 4.2 -------------------------------------------------------------------------
+
+select a.sttct, a.tenct, a.kinhphi
+from congtrinh a, thietke b, kientrucsu c
+where c.ngaysinh = (
+  select min(ngaysinh)
+  from kientrucsu
+)
+      and a.sttct = b.sttct
+      and b.mskts = c.mskts;
+      
+-- 4.3 -------------------------------------------------------------------------
+
+select a.mskts, a.hotenkts
+from kientrucsu a, thietke b, congtrinh c, chunhan d
+where a.mskts = b.mskts
+      and b.sttct = c.sttct
+      and c.msch = d.msch
+      and d.diachichu = '101 Hai Ba Trung';
+
+-- 4.4 -------------------------------------------------------------------------
+
+select distinct a.mskts, a.hotenkts
+from kientrucsu a, thietke b, congtrinh c
+where extract(year from current_date) - extract(year from a.ngaysinh) < 40
+      and a.mskts = b.mskts
+      and b.sttct = c.sttct
+      and 3 <= (
+        select count(d.sttct)
+        from congtrinh d
+        where c.msct = d.msct
+      );
+      
+-- 4.5 -------------------------------------------------------------------------
+
+select distinct a.mscn, a.hotencn
+from congnhan a, thamgia b, congtrinh c, thamgia d, congtrinh e
+where a.mscn = b.mscn
+      and b.sttct = c.sttct
+      and a.mscn = d.mscn
+      and d.sttct = e.sttct
+      and c.tinhthanh <> e.tinhthanh;
+
+-- 4.6 -------------------------------------------------------------------------
+
+create view temp6 as
+select a.tinhthanh, count(b.mscn) socn
+from congtrinh a, thamgia b
+where a.sttct = b.sttct
+group by a.tinhthanh;
+
+select tinhthanh
+from temp6
+where socn = (
+  select max(socn)
+  from temp6
+);
+
+drop view temp6;
+
+-- 4.7 -------------------------------------------------------------------------
+
+select a.tinhthanh
+from congtrinh a, thietke b, kientrucsu c
+where a.sttct = b.sttct
+      and b.mskts = c.mskts
+      and c.hotenkts = 'Le Kim Dung'
+      and b.thulao = (
+        select min(d.thulao)
+        from thietke d, kientrucsu e
+        where d.mskts = e.mskts
+              and e.mskts = c.mskts
+      );
+
+-- 4.8 -------------------------------------------------------------------------
+
+select distinct a.hotenkts, e.hotencn
+from kientrucsu a, thietke b, congtrinh c, thamgia d, congnhan e
+where a.mskts = b.mskts
+      and b.sttct = c.sttct
+      and c.sttct = d.sttct
+      and d.mscn = e.mscn
+      and a.ngaysinh < e.ngaysinh;
+      
+-- 4.9 -------------------------------------------------------------------------
+
+select a.mscn, a.hotencn, count(b.sttct) soct
+from congnhan a, thamgia b
+where a.mscn = b.mscn
+      and extract(month from a.ngaysinh) = 5
+group by a.mscn, a.hotencn
+order by a.mscn;
+
+-- 4.10 ------------------------------------------------------------------------
+
+select a.sttct, a.tenct, count(b.mscn) socn, sum(case when c.phai = 'Nam' then 1 else 0 end) socnnam, sum(case when c.phai = 'Nu' then 1 else 0 end) socnnu
+from congtrinh a, thamgia b, congnhan c
+where a.sttct = b.sttct
+      and b.mscn = c.mscn
+group by a.sttct, a.tenct
+having count(b.mscn) > 2
+order by a.sttct;
